e0259451aa6c3ed1e58b316dfd9de49907214fa6
mathiaascn <mathiaas@live.no>
2022-09-29 23:21:59 +0200

    simplyfy

--- content/body.php
+++ content/body.php
@@ -1,33 +1,26 @@
-<div class="wrapper">
-    <!-- <div class="row">
-        <section class="corner"></section>
-        <section class="zeplin ad_space"></section>
-        <section class="corner"></section>
-    </div> -->
-    <div class="row main_row">
-        <section class="skyscraper ad_space"></section>
-        <section class="content">
-            <div class="row">
-                <h1 style="font-family:beaufort;font-size:48px;color:gold;background:black;">TEAMFIGHT TACTICS STRAT ROULETTE</h1>
-                <div class="difficulty_container">
-                    <div class="d_btn d1">
-                        <img src="/tftstratroulette/res/d1.png" alt="easy_difficulty">
-                    </div>
-                    <div class="d_btn d2">
-                        <img src="/tftstratroulette/res/d2.png" alt="medium_difficulty">
-                    </div>
-                    <div class="d_btn d3">
-                        <img src="/tftstratroulette/res/d3.png" alt="hard_difficulty">
-                    </div>
+<div class="app">
+    <div class="ad_space l_ad"></div>
+    <div class="ad_space r_ad"></div>
+    <div class="main_container">
+        <div class="row">
+            <h1 style="font-family:beaufort;font-size:48px;color:gold;background:black;">TEAMFIGHT TACTICS STRAT ROULETTE</h1>
+            <div class="difficulty_container">
+                <div class="d_btn d1">
+                    <img src="/tftstratroulette/res/d1.png" alt="easy_difficulty">
+                </div>
+                <div class="d_btn d2">
+                    <img src="/tftstratroulette/res/d2.png" alt="medium_difficulty">
+                </div>
+                <div class="d_btn d3">
+                    <img src="/tftstratroulette/res/d3.png" alt="hard_difficulty">
                 </div>
             </div>
-            <div class="row">
-                <div class="output_container"></div>
-            </div>
-            <div class="row">
-                <div class="refresh_container"></div>
-            </div>
-        </section>
-        <section class="skyscraper ad_space"></section>
+        </div>
+        <div class="row">
+            <div class="output_container"></div>
+        </div>
+        <div class="row">
+            <div class="refresh_container"></div>
+        </div>
     </div>
 </div>
