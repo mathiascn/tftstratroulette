f08c510a2e9ee592e1cf5ab7a852336020541765
mathiaascn <mathiaas@live.no>
2022-10-02 00:27:06 +0200

    1

--- content/inc/db.php
+++ content/inc/db.php
@@ -1,13 +1,12 @@
 <?php
 class db {
-
     protected $connection;
 	protected $query;
     protected $show_errors = TRUE;
     protected $query_closed = TRUE;
 	public $query_count = 0;
 
-	public function __construct($dbhost = 'localhost', $dbuser = 'root', $dbpass = 'Mathias123!', $dbname = 'tftstrat', $charset = 'utf8mb4') {
+	public function __construct($dbhost = 'localhost', $dbuser = 'root', $dbpass = '123', $dbname = 'tftstrat', $charset = 'utf8mb4') {
 		$this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 		if ($this->connection->connect_error) {
 			$this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
